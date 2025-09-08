4e5d70eabdc41f17013ebd63b371ef129f26f3b2
Ankita Kumari <ankita@Ankitas-MacBook-Air.local>
2025-09-08 12:09:56 +0530

    episode 01

---
+++ NamasteReact/app.js
@@ -0,0 +1,19 @@
+// createElement returns an Object 
+const heading = React.createElement('h1', {id: 'heading'}, "Hello world from React!")         
+const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(heading)
+
+console.log(heading);
+
+const structure = React.createElement('div', { id: 'parent' },  
+  [React.createElement('div', { id: 'child' },
+    [React.createElement('h1', {}, "Hi, I'm an h1 tag"), 
+      React.createElement('h2', {}, "Hi, I'm an h2 tag")
+    ]
+   ),
+  React.createElement('div', { id: 'child-2' },
+    [React.createElement('h1', {}, "Hi, I'm an h1 tag"), 
+      React.createElement('h2', {}, "Hi, I'm an h2 tag")
+    ]
+   )])
+root.render(structure)
