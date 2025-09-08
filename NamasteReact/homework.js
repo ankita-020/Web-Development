52aa6d75342ed8892eb451f0bd85143b4498d7da
Ankita Kumari <ankita@Ankitas-MacBook-Air.local>
2025-09-08 12:44:32 +0530

    Added package.json for react

--- NamasteReact/homework.js
+++
@@ -1,6 +0,0 @@
-/****** 
- * Learn about CDN - what is CDN, why do we use cdn, is there any other way?
- * what is cross-origin
- * Can we change the order of script in HTML file
- * Benefits of React - React is JS library which can be applied in any existing app, for any part of app easily
- */
