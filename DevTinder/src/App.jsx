6941862cda68075ed6d406129f13700a1b325efe
Ankita Kumari <ankitakri2596@gmail.com>
2025-09-26 18:29:53 +0530

    Restructured folders in DevTinder

--- DevTinder/src/App.jsx
+++ DevTinder/src/App.jsx
@@ -1,6 +1,7 @@
-import Body from "./Body";
-import Profile from "./Profile";
-import Login from "./Login";
+import Body from "./components/Body";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+import Feed from "./components/Feed";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Body />}>
+            <Route path="/" element={<Feed />} />
             <Route path="/login" element={<Login />} />
             <Route path="/profile" element={<Profile />} />
           </Route>
